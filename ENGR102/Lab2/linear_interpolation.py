f461327e7e4c022b0a6f640e0188e333ebefa766
SumitNalavade <vs.nalavade2003@gmail.com>
2022-08-30 11:23:23 -0500

    Complete linear interpolation

--- ENGR102/Lab2/linear_interpolation.py
+++ ENGR102/Lab2/linear_interpolation.py
@@ -10,18 +10,33 @@
 # Assignment:   Lab 2.8
 # Date:         30 08 2022
 
+from math import pi
+
 #Part 1
-initialTime = 10
-initialPosition = 2026
+def calculateTotalDistance(testTime):
+    initialTime = 10
+    initialPosition = 2026
 
-finalTime = 55
-finalPosition = 23026
+    finalTime = 55
+    finalPosition = 23026
 
-#Given time ('x' value to interpolate)
-testTime = 25
+    #Calculate the position of the iss at a given time (testTime)
+    testPosition = initialPosition + ((finalPosition - initialPosition)/(finalTime - initialTime) * (testTime - initialTime))
 
-#Calculate the position of the iss at a given time (testTime)
-testPosition = initialPosition + ((finalPosition - initialPosition)/(finalTime - initialTime) * (testTime - initialTime))
+    return testPosition
 
-print(f'For t = {testTime} minutes, the position p = {testPosition} kilometers')
+#Part 2
+def calculateDistanceFromHouston(testTime):
+    earthRadius = 6745
+    earthCircumference = 2 * pi * earthRadius
 
+    totalDistance = calculateTotalDistance(testTime)
+    distanceFromHouston = totalDistance % earthCircumference
+
+    return distanceFromHouston
+
+print("Part 1:")
+print(f'For t = {25} minutes, the position p = {calculateTotalDistance(25)} kilometers')
+
+print("Part 2:")
+print(f'For t = {300} minutes, the position p = {calculateDistanceFromHouston(300)} kilometers')
